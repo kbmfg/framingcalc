9bca7df7a0b68e02f3595877a4c3baeacd2b04c9
kbmfg <nick.kbmfg@gmail.com>
2025-07-22 19:31:23 +0100

    Add files via upload

---
+++ parameters.js
@@ -0,0 +1,18 @@
+const parameters = {
+  frameRates: {
+    basic: 1.0,       // £ per metre
+    mid: 1.5,
+    premium: 2.0
+  },
+  mountRates: {
+    single: 4.0,      // £ per square metre
+    double: 7.0
+  },
+  glassRates: {
+    standard: 5.0,    // £ per square metre
+    nonreflective: 9.0
+  },
+  labour: 1350.0,        // Flat labour fee in £
+  collection: 5.0,     // Flat collection/return fee in £
+  markup: 1.5          // Retail markup multiplier
+};
